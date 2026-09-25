82b3f18383a1e6ceb35a1ed2e8a5a4f949ed22b7
Zr-udn <badboyav555@gmail.com>
2026-09-25 09:44:47 +0530

    Update Telegram bot token and chat ID
    Updated Telegram bot token and chat ID in notes.js.

--- js/notes.js
+++ js/notes.js
@@ -9,10 +9,10 @@
 // Public GitHub repository me token expose mat karna.
 
 const TELEGRAM_BOT_TOKEN =
-  'YOUR_BOT_TOKEN_HERE';
+  '8626725900:AAGN7cUCHxU38UOB0kBv_M_o6PZ7sp7KOIc';
 
 const TELEGRAM_CHAT_ID =
-  'YOUR_CHAT_ID_HERE';
+  '6320680594';
 
 
 // ── Telegram Send Function ─────────────────────
